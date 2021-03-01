3aec56dad78d44ed15c68e72817916f951f2609c
ftandas <ftandas@my.bcit.ca>
2021-03-01 12:40:50 -0800

    seafloor

---
+++ static_stuff/seafloor/seafloor.js
@@ -0,0 +1,31 @@
+function seafloorUI(){
+    return `<div style='
+        position: static;
+        min-width: 100%;
+        min-height: 100px;
+        bottom: 0;
+    '>
+    <svg width="375" height="89" viewBox="0 0 375 89" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M375 89V33.7973C375 32.8432 370.725 30.4033 353.625 28.277C332.25 25.6191 337.5 9.26275 311.25 1.28902C285 -6.6847 271.125 24.8013 256.875 28.277C242.625 31.7528 219.75 24.8013 204.375 15.8053C189 6.80928 156 19.4855 138.75 24.8013C121.5 30.1171 107.625 14.3741 83.625 15.8053C59.625 17.2365 39 27.2547 33 35.6374L0 33.7973V89H375Z" fill="#EED07F"/>
+    <ellipse rx="2.5" ry="3" transform="matrix(-1 0 0 1 67.5 64)" fill="#D5BA73"/>
+    <ellipse rx="2.5" ry="3" transform="matrix(-1 0 0 1 29.5 70)" fill="#D5BA73"/>
+    <ellipse rx="2.5" ry="3" transform="matrix(-1 0 0 1 17.5 51)" fill="#D5BA73"/>
+    <ellipse rx="2.5" ry="3" transform="matrix(-1 0 0 1 109.5 77)" fill="#D5BA73"/>
+    <ellipse rx="2.5" ry="3" transform="matrix(-1 0 0 1 86.5 34)" fill="#D5BA73"/>
+    <ellipse rx="2.5" ry="3" transform="matrix(-1 0 0 1 154.5 34)" fill="#D5BA73"/>
+    <ellipse rx="2.5" ry="3" transform="matrix(-1 0 0 1 171.5 67)" fill="#D5BA73"/>
+    <ellipse rx="2.5" ry="3" transform="matrix(-1 0 0 1 228.5 34)" fill="#D5BA73"/>
+    <ellipse rx="2.5" ry="3" transform="matrix(-1 0 0 1 279.5 67)" fill="#D5BA73"/>
+    <ellipse rx="2.5" ry="3" transform="matrix(-1 0 0 1 333.5 67)" fill="#D5BA73"/>
+    <ellipse rx="2.5" ry="3" transform="matrix(-1 0 0 1 359.5 40)" fill="#D5BA73"/>
+    <ellipse rx="2.5" ry="3" transform="matrix(-1 0 0 1 324.5 37)" fill="#D5BA73"/>
+    <ellipse rx="2.5" ry="3" transform="matrix(-1 0 0 1 216.5 48)" fill="#D5BA73"/>
+    <ellipse rx="2.5" ry="3" transform="matrix(-1 0 0 1 211.5 83)" fill="#D5BA73"/>
+    <ellipse rx="2.5" ry="3" transform="matrix(-1 0 0 1 287.5 28)" fill="#D5BA73"/>
+    <ellipse rx="2.5" ry="3" transform="matrix(-1 0 0 1 307.5 52)" fill="#D5BA73"/>
+    <ellipse rx="2.5" ry="3" transform="matrix(-1 0 0 1 250.5 52)" fill="#D5BA73"/>
+    </svg>
+    </div>`
+}
+
+export const seafloor = seafloorUI();

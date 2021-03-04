37280be411371691e9af3f37915e37f91f129c91
ftandas <ftandas@my.bcit.ca>
2021-03-04 14:54:48 -0800

    seafloor and button complete for comp

--- static_stuff/seafloor/seafloor.js
+++ static_stuff/seafloor/seafloor.js
@@ -1,9 +1,9 @@
 function seafloorUI(){
-    return `<div style='
-        position: static;
-        min-width: 100%;
-        min-height: 100px;
-        bottom: 0;
+    return `<div styel = '
+    width: 375px;
+    height: 89px;
+    left: 0px;
+    top: 4123px;
     '>
     <svg width="375" height="89" viewBox="0 0 375 89" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M375 89V33.7973C375 32.8432 370.725 30.4033 353.625 28.277C332.25 25.6191 337.5 9.26275 311.25 1.28902C285 -6.6847 271.125 24.8013 256.875 28.277C242.625 31.7528 219.75 24.8013 204.375 15.8053C189 6.80928 156 19.4855 138.75 24.8013C121.5 30.1171 107.625 14.3741 83.625 15.8053C59.625 17.2365 39 27.2547 33 35.6374L0 33.7973V89H375Z" fill="#EED07F"/>

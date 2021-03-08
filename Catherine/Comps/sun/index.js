46a2b74929330fff23df0274ce0e40099f2f0ae1
catherinelamyw <clam166@my.bcit.ca>
2021-03-08 01:21:04 -0800

    Added styling and scroll function to trash graphics

--- Catherine/Comps/sun/index.js
+++ Catherine/Comps/sun/index.js
@@ -1,14 +1,14 @@
 function RotateSunUI(src="http://placekitten.com/300/300") {
   return `
-    <div style="
+    <div class="sundiv" style="
       height:5em;
       width:5em;
       background-color:#333;
     ">
-      <img src="${src}"
+      <img class="sunimg rotate" src="${src}"
       style="
-        width:150px;
-        height:150px;
+        height:5em;
+        width:5em;
       ">
     </div>
   `

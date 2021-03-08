1fd179a29ca835eadbdbf39fab8cb43632948d8c
EliasMatteo <eflamencosolito@my.bcit.ca>
2021-03-08 00:31:26 -0800

    added my files that allow my comps to show and gave some file layout guidance for when we do the layout for the final product/ these adjustments spanned from the HTMl to the JS

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// THIS IS WHERE WE SHALL PUT RANDOM FUNCTIONS THAT HOPEFULLY WORK 
+
+// TEAM MOTTO
+
+// I like turtles because they’re so chill. They don’t hurt anyone. 
+//They’re just like, “Hey man, I want to swim, and maybe eat some lettuce. But I’m gonna take my time getting there, I’m not in a rush. Because I’m a turtle.”
+
+// COMPONENT LINKS / FOR THE MOMENT IF YOU HAVE ANY TO ATTACH JUST PUT EM AS #MAIN AND WE SHALL ORGANIZE LATER
+
+    // ELIAS MATTEO COMPONENT LINKS
+    document.querySelector('#main').innerHTML += WavesUI()
+    document.querySelector('#main').innerHTML += FishUI()
+    document.querySelector('#main').innerHTML += Fish2UI()
+    document.querySelector('#main').innerHTML += Fish3UI()
+    document.querySelector('#main').innerHTML += SchoolFishUI()
+    document.querySelector('#main').innerHTML += SchoolFish2UI()
+    document.querySelector('#main').innerHTML += SchoolFish3UI()
+    document.querySelector('#main').innerHTML += SchoolFish4UI()
+    //
+
+    // FARBOD COMPONENT LINKS
+
+    //
+
+    // CATHERINE COMPONENT LINKS
+
+    //
+
+
+
+// PUT COMPONENT LINKS ABOVE ME PLEASE 
+
+/////////////
+
+// FUNCTIONS AND OTHER SITE BEHAVIOURS THAT COULD NOT BE PUT INTO COMPS 
+
+
+
+
+
+
+
+
+
+
+
+
+// PLEASE PUT FUNCTIONS AND JS ABOVE THANKS AMIGO-KUN

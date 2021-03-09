a88a1e7725b54c2db525cd32aa372d53dc514593
catherinelamyw <clam166@my.bcit.ca>
2021-03-08 21:15:11 -0800

    started to link components to divs

--- js/main.js
+++ js/main.js
@@ -22,55 +22,54 @@
     //
 
     // FARBOD COMPONENT LINKS
-    document.querySelector('#main').innerHTML += popupUI()
-    document.querySelector('#main').innerHTML += buttonbottomUI()
-    document.querySelector('#main').innerHTML += coralreefUI()
-    document.querySelector('#main').innerHTML += litterUI()
-    document.querySelector('#main').innerHTML += litteringUI()
-    document.querySelector('#main').innerHTML += mantrashUI()
-    document.querySelector('#main').innerHTML += oildrumUI()
-    document.querySelector('#main').innerHTML += recyclingUI()
-    document.querySelector('#main').innerHTML += seafloorUI()
-    document.querySelector('#main').innerHTML += turtleoilUI()
+    document.querySelector('#title').innerHTML += popupUI()
+    document.querySelector('#floor').innerHTML += buttonbottomUI()
+    document.querySelector('#save').innerHTML += coralreefUI()
+    document.querySelector('#help').innerHTML += litterUI()
+    document.querySelector('#help').innerHTML += litteringUI()
+    document.querySelector('#help').innerHTML += mantrashUI()
+    document.querySelector('#help').innerHTML += recyclingUI()
+    document.querySelector('#floor').innerHTML += oildrumUI()
+    document.querySelector('#floor').innerHTML += seafloorUI()
+    document.querySelector('#floor').innerHTML += turtleoilUI()
 
 
     //
 
     // CATHERINE COMPONENT LINKS
-    document.querySelector('#main').innerHTML += RotateSunUI()
+    document.querySelector('#title').innerHTML += RotateSunUI()
 
-    document.querySelector('#main').innerHTML += HeadingUI("Sea turtles are endangered. Why should we care?")
-    document.querySelector('#main').innerHTML += TextboxUI("Each sea turtle species uniquely affects the health, diversity, habitat and functionality of its environment")
-    document.querySelector('#main').innerHTML += TextboxUI("Hawksbill sea turtles control sponge distribution. ")
-    document.querySelector('#main').innerHTML += TextboxUI("They are one of the few hebivores that eat sponges. ")
-    document.querySelector('#main').innerHTML += TextboxUI("With fewer sponges in the reefs, corals have a chance to grow, which is the home for these turtles")
+    document.querySelector('#intro').innerHTML += HeadingUI("Sea turtles are endangered. Why should we care?")
+    document.querySelector('#intro').innerHTML += TextboxUI("Each sea turtle species uniquely affects the health, diversity, habitat and functionality of its environment")
+    document.querySelector('#intro').innerHTML += TextboxUI("Hawksbill sea turtles control sponge distribution. ")
+    document.querySelector('#intro').innerHTML += TextboxUI("They are one of the few hebivores that eat sponges. ")
+    document.querySelector('#intro').innerHTML += TextboxUI("With fewer sponges in the reefs, corals have a chance to grow, which is the home for these turtles")
     
-    document.querySelector('#main').innerHTML += TextboxUI("Sea turtles are one of the key species for a healthy ocean because they influence many other species around them. ")
-    document.querySelector('#main').innerHTML += TextboxUI("Sea turtle hatchlings have a big impact on the environment. ")
-    document.querySelector('#main').innerHTML += TextboxUI("They are a food source for many animals. ")
-    document.querySelector('#main').innerHTML += TextboxUI("They leave behind nutrients good for coastal vegitation. ")
+    document.querySelector('#whycare').innerHTML += TextboxUI("Sea turtles are one of the key species for a healthy ocean because they influence many other species around them. ")
+    document.querySelector('#whycare').innerHTML += TextboxUI("Sea turtle hatchlings have a big impact on the environment. ")
+    document.querySelector('#whycare').innerHTML += TextboxUI("They are a food source for many animals. ")
+    document.querySelector('#whycare').innerHTML += TextboxUI("They leave behind nutrients good for coastal vegitation. ")
     
-    document.querySelector('#main').innerHTML += HeadingUI("Why are they endangered?")
-    document.querySelector('#main').innerHTML += TextboxUI("1. Slaughtered by poachers. 2. Pollution from humans make their homes inhabitable. 3. Warmer sand temperatures produce more female hatchlings.")
+    document.querySelector('#endanger').innerHTML += HeadingUI("Why are they endangered?")
+    document.querySelector('#endanger').innerHTML += TextboxUI("1. Slaughtered by poachers. 2. Pollution from humans make their homes inhabitable. 3. Warmer sand temperatures produce more female hatchlings.")
     
-    document.querySelector('#main').innerHTML += HeadingUI("How can we help?")
-    document.querySelector('#main').innerHTML += TextboxUI("Instead of using plastic bags for shopping, let’s try to bring a cloth bag every time we go out.")
-    document.querySelector('#main').innerHTML += TextboxUI("Instead of buying snack-sized food packages, let’s try to buy bulk food using glass containers.")
+    document.querySelector('#help').innerHTML += HeadingUI("How can we help?")
+    document.querySelector('#help').innerHTML += TextboxUI("Instead of using plastic bags for shopping, let’s try to bring a cloth bag every time we go out.")
+    document.querySelector('#help').innerHTML += TextboxUI("Instead of buying snack-sized food packages, let’s try to buy bulk food using glass containers.")
     
-    document.querySelector('#main').innerHTML += HeadingUI("Can we really save the sea turtles from extinction?")
-    document.querySelector('#main').innerHTML += TextboxUI("The less plastic you use, the smaller the chance of it being eaten by animals that mistake it as food or trapping animals. Every little decision you make can combine to make a huge difference in the ecosystem")
-    document.querySelector('#main').innerHTML += TextboxUI("Instead of dead coral reefs... let’s preserve living coral reefs.")
+    document.querySelector('#save').innerHTML += HeadingUI("Can we really save the sea turtles from extinction?")
+    document.querySelector('#save').innerHTML += TextboxUI("The less plastic you use, the smaller the chance of it being eaten by animals that mistake it as food or trapping animals. Every little decision you make can combine to make a huge difference in the ecosystem")
+    document.querySelector('#save').innerHTML += TextboxUI("Instead of dead coral reefs... let’s preserve living coral reefs.")
     
-    document.querySelector('#main').innerHTML += HeadingUI("Now where does our app come in ?")
-    document.querySelector('#main').innerHTML += TextboxUI("We want to raise awareness for the cruel things that are happening to these animals.")
-    document.querySelector('#main').innerHTML += TextboxUI("And to help educate younger minds of such things for a brighter future...")
-    document.querySelector('#main').innerHTML += TextboxUI("not only for these sea turtles but for all animals, and with this app we can take a step in the right direction. ")
-    document.querySelector('#main').innerHTML += TextboxUI("Through a fun and interactive story that will help guide the user & provide a robust learning experience.")
+    document.querySelector('#ending').innerHTML += HeadingUI("Now where does our app come in ?")
+    document.querySelector('#ending').innerHTML += TextboxUI("We want to raise awareness for the cruel things that are happening to these animals.")
+    document.querySelector('#ending').innerHTML += TextboxUI("And to help educate younger minds of such things for a brighter future...")
+    document.querySelector('#ending').innerHTML += TextboxUI("not only for these sea turtles but for all animals, and with this app we can take a step in the right direction. ")
+    document.querySelector('#ending').innerHTML += TextboxUI("Through a fun and interactive story that will help guide the user & provide a robust learning experience.")
 
-    document.querySelector('#main').innerHTML += HeadingUI("Thank you for reading!")
-
-    document.querySelector('#main').innerHTML += FallingTrashUI("../../img/bottle.png")
-    document.querySelector('#main').innerHTML += FallingTrashUI("../../img/can.png")
+    document.querySelector('#floor').innerHTML += HeadingUI("Thank you for reading!")
+    document.querySelector('#floor').innerHTML += FallingTrashUI("../../img/bottle.png")
+    document.querySelector('#floor').innerHTML += FallingTrashUI("../../img/can.png")
 
     //
 

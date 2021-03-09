c54ad0a756db7be8017a75df95fed194fa93c34c
ftandas <ftandas@my.bcit.ca>
2021-03-08 21:17:36 -0800

    pop up complete and coralreef image made

---
+++ Farbod/Comps/coralreef/index.js
@@ -0,0 +1,16 @@
+function coralreefUI() {
+    return `
+    <div style='
+        justify-content: center;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        '>
+        <img style='
+            width: 370px;
+            height: 270px;
+            border: solid white 3px;'
+            src='/img/coralreefboth.png'/>
+    </div>
+    `
+}
--- Farbod/Comps/info-pop-up/index.js
+++ Farbod/Comps/info-pop-up/index.js
@@ -8,19 +8,15 @@ function popupUI() {
 
 <div>
     <div>
-        <button style='
-        top: 8px;
-        left: 8px;
+        <button type='button' onclick='vis()' 
+        style='
         overflow: hidden;
-        position: relative;
         width: 40px;
         height: 40px;
         border-radius: 30%;
         background-color: #FFE9A7;
         box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-        border: none;
-        '
-         onclick='vis()'>
+        border: none;'>
             <div style='
             font-family: Varela Round, sans-serif;
             font-weight: bold;
@@ -29,36 +25,35 @@ function popupUI() {
             padding-top: 11%;
             font-size: 26px;
             color: #88A7BC;
-            '>
-            i</div>
+            '>i</div>
         </button>
     </div>
-</div>
-<div id="cent" style='
-align-items: center;
-justify-content: center;
-display: none;
-flex-direction: column;
-'>
-    <div style='
-    width: 290px;
-    height: 645px;
-    border-radius: 12px;
-    background-color: white;
-    border: solid 3px #FFE9A7;
-    justify-content: center;
-    display: flex;
+    </div>
+    <div id="cent" style='
     align-items: center;
+    justify-content: center;
     flex-direction: column;
-    margin-top: 40px;
+    display: none;
     '>
+        <div style='
+        width: 290px;
+        height: 645px;
+        border-radius: 12px;
+        background-color: white;
+        border: solid 3px #FFE9A7;
+        justify-content: center;
+        align-items: center;
+        display: flex;
+        flex-direction: column;
+        margin-top: 40px;
+        '>
         <div style='
         color: #242C3C;
         font-size: 16px;
+        display: flex;
         font-family: Roboto, sans-serif;
         text-align: center;
         justify-content: center;
-        display: flex;
         align-items: center;
         flex-direction: column;
         font-weight: 200;
@@ -89,10 +84,10 @@ flex-direction: column;
         <br>
         <div><button style='
         text-align: center;
+        display: flex;
         align-items: center;
         flex-direction: column;
         justify-content: center;
-        display: flex;
         background-color: #FFE9A7;
         border-radius: 244px;
         width: 244px;
@@ -102,8 +97,8 @@ flex-direction: column;
         font-size: 16px;
         color: #242C3C;
         border: none;
-        font-family: Roboto, sans-serif;
-        '>
+        font-family: Roboto, sans-serif;'
+        onclick="invis()">
         Foundation Link</button></div>
     </div>
 </div>
@@ -111,12 +106,17 @@ flex-direction: column;
 }
 
 function vis() {
-    var x = document.getElementById('#cent');
-    if (x.style.display === 'none') {
-    x.style.display = 'flex';
-} 
-    else {
-    x.style.display = 'none';
-    }
+    document.getElementById('cent').style.display = "flex"
 }
+
+function invis() {
+    document.getElementById('cent').style.display = "none"
+}
+    //     if (buttonC.click.display === "none") {
+//     poppy.style.display = "flex";
+// } 
+//     else {
+//     poppy.style.display = "none";
+//     }
+// }
 

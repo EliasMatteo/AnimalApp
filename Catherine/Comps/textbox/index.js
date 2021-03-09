a61432346b9d14c14dc698801e755e45246bb758
catherinelamyw <clam166@my.bcit.ca>
2021-03-08 22:14:33 -0800

    added a textbox for lists and made width adjustable

--- Catherine/Comps/textbox/index.js
+++ Catherine/Comps/textbox/index.js
@@ -1,18 +1,44 @@
-function TextboxUI(p="Each sea turtle species uniquely affects the health, diversity, habitat and functionality of its environment") {
+function TextboxUI(
+  width="300",
+  p="Each sea turtle species uniquely affects the health, diversity, habitat and functionality of its environment") {
   return `
   <p style="
     border-radius:25px;
     font-family:sans-serif;
     font-size:18px;
     font-weight:500;
-    width:100vw;
-    max-width:300px;
+    width:300px;
+    max-width:${width}px;
     background-color:white;
     display:flex;
     align-content:center;
     padding:20px;
     overflow-wrap:anywhere;
   "> ${p}
+  </p>
+  `
+}
+
+function TextboxUIlist(
+  one="1. Slaughtered by poachers.",
+  two="2. Pollution from humans make their homes inhabitable.",
+  three="3. Warmer sand temperatures produce more female hatchlings."
+  ) {
+  return `
+  <p style="
+    border-radius:25px;
+    font-family:sans-serif;
+    font-size:18px;
+    font-weight:500;
+    width:300px;
+    max-width:300px;
+    background-color:white;
+    display:flex;
+    align-content:center;
+    padding:20px;
+    overflow-wrap:anywhere;
+    z-index:1;
+  "> ${one}<br>${two}<br>${three}
   </p>
   `
 }
